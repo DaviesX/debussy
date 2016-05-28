b9dc52da197cbdc81bcb327b5607e81a1611d3f4
Chifeng Wen <daviesx66@gmail.com>
2016-05-27 20:03:48 -0700

    added file prototypes

--- src/debussy/noteplayer.c
+++ src/debussy/noteplayer.c
@@ -3,20 +3,17 @@
 #include <noteplayer.h>
 
 #define WHOLE_NOTE      16
-
 #define FREQ_A4         440
 
-static float g_freq[88];
-
-static float note_player_get_freq(uint8_t semi)
+static float note_player_get_freq(struct note_player* self, uint8_t semi)
 {
-        return g_freq[semi];
+        return self->freq[semi];
 }
 
-void note_player_init()
+void note_player_init(struct note_player* self)
 {
         int8_t i;
         for (i = -48; i <= 38; i ++) {
-                g_freq[i + 48] = FREQ_A4*pow(2, (double) i/12.0f);
+                self->freq[i + 48] = FREQ_A4*pow(2, (double) i/12.0f);
         }
 }
--- src/debussy/noteplayer.h
+++ src/debussy/noteplayer.h
@@ -2,9 +2,16 @@
 #define MUSICPLAYER_H_INCLUDED
 
 /*
+ * <note_player>
+ */
+struct note_player {
+        float   freq[88];
+};
+
+/*
  * <note_player> public
  */
-void note_player_init();
+void note_player_init(struct note_player* self);
 
 
 #endif // MUSICPLAYER_H_INCLUDED
