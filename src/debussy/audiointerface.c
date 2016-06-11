bdefc3ea97bb8916a1238dbc688bed3a4fcb9219
Chifeng Wen <daviesx66@gmail.com>
2016-06-11 13:19:11 -0700

    refactored audioif to support different architecture.

--- src/debussy/audiointerface.c
+++ src/debussy/audiointerface.c
@@ -7,7 +7,7 @@
 #define PIN     3
 
 
-void audioif_du()
+void audioif_du_avr()
 {
         SET_BIT(DDR, PIN);
         uint16_t i;
@@ -21,7 +21,43 @@ void audioif_du()
         CLR_BIT(PORT, PIN);
 }
 
-static void audioif_set_period(uint16_t period)
+void audioif_on_avr()
+{
+        SET_BIT(DDR, PIN);
+        sei();
+}
+
+void audioif_off_avr()
+{
+        cli();
+        CLR_BIT(DDR, PIN);
+}
+
+static void audioif_keep(uint16_t ms)
+{
+        TCCR0 = 2;
+        while (ms--) {
+                TCNT0 = 255;
+                SET_BIT(TIFR, TOV0);
+                while (!GET_BIT(TIFR, TOV0));
+        }
+        TCCR0 = 0;
+}
+
+void audioif_output_1bit_sw_avr(uint16_t freq, uint16_t duration, float volume)
+{
+        uint16_t period = 2*1000000UL/freq/2;
+        uint32_t n = (uint32_t) duration/(uint32_t) period * 500UL;
+        uint16_t i;
+        for (i = 0; i < n; i ++) {
+                SET_BIT(PORT, PIN);
+                audioif_keep(period);
+                CLR_BIT(PORT, PIN);
+                audioif_keep(period);
+        }
+}
+
+static void __audioif_set_period_avr(uint16_t period)
 {
         TCCR1A = 0;
         TCCR1B = 0;
@@ -43,58 +79,30 @@ ISR(TIMER1_COMPA_vect)
         flip = !flip;
 }
 
-void audioif_on()
+void audioif_output_1bit_avr(uint16_t freq, uint16_t duration, float volume)
 {
-        SET_BIT(DDR, PIN);
-        sei();
-}
-
-void audioif_off()
-{
-        cli();
-        CLR_BIT(DDR, PIN);
-}
-
-static void audioif_keep(uint16_t ms)
-{
-        TCCR0 = 2;
-        while (ms--) {
-                TCNT0 = 255;
-                SET_BIT(TIFR, TOV0);
-                while (!GET_BIT(TIFR, TOV0));
-        }
-        TCCR0 = 0;
-}
-
-void audioif_square_wave_software(uint16_t freq, uint16_t duration, float volume)
-{
-        audioif_on();
-
-        uint16_t period = 2*1000000UL/freq/2;
-        uint32_t n = (uint32_t) duration/(uint32_t) period * 500UL;
-        uint16_t i;
-        for (i = 0; i < n; i ++) {
-                SET_BIT(PORT, PIN);
-                audioif_keep(period);
-                CLR_BIT(PORT, PIN);
-                audioif_keep(period);
-        }
-
-        audioif_off();
-}
-
-void audioif_square_wave(uint16_t freq, uint16_t duration, float volume)
-{
-        audioif_on();
-
         uint16_t period = 1000000UL/freq/2;
         uint16_t dutycycle = 255*volume;
         OCR0 = dutycycle;
 
-        audioif_set_period(period);
+        __audioif_set_period_avr(period);
         audioif_keep(duration*1000);
-
-        audioif_off();
 }
 
+void audioif_output_24bit_wave_avr(float fn)
+{
+}
+
+#else
+void audioif_on_x64()
+{
+}
+
+void audioif_off_x64()
+{
+}
+
+void audioif_output_24bit_wave_x64(float fn)
+{
+}
 #endif // ARCH_X86_64
--- src/debussy/audiointerface.h
+++ src/debussy/audiointerface.h
@@ -4,11 +4,26 @@
 /*
  * <audiointerface> public
  */
-void audioif_du();
-void audioif_on();
-void audioif_square_wave_software(uint16_t freq, uint16_t duration, float volume);
-void audioif_square_wave(uint16_t freq, uint16_t duration, float volume);
-void audioif_off();
+void audioif_du_avr();
+void audioif_on_avr();
+void audioif_off_avr();
+void audioif_output_1bit_sw_avr(uint16_t freq, uint16_t duration, float volume);
+void audioif_output_1bit_avr(uint16_t freq, uint16_t duration, float volume);
+void audioif_output_24bit_wave_avr(float fn);
+
+void audioif_on_x64();
+void audioif_off_x64();
+void audioif_output_24bit_wave_x64(float fn);
+
+#ifndef ARCH_X86_64
+#  define audioif_on                    audioif_on_avr
+#  define audioif_off                   audioif_off_avr
+#  define audioif_output_24bit_wave     audioif_output_24bit_wave_avr
+#else
+#  define audioif_on                    audioif_on_x64
+#  define audioif_off                   audioif_off_x64
+#  define audioif_output_24bit_wave     audioif_output_24bit_wave_x64
+#endif // ARCH_X86_64
 
 
 #endif // AUDIOINTERFACE_H_INCLUDED
