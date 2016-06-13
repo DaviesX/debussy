48cf433001ba553e0b04a96f0291aa3a3ccabae5
Chifeng Wen <daviesx66@gmail.com>
2016-06-12 22:46:51 -0700

    created unit test projects.

---
+++ src/utest/utest64.c
@@ -0,0 +1,7 @@
+#include <avr.h>
+
+
+int main()
+{
+        return 0;
+}
--- src/utest/utestavr.c
+++ src/utest/utestavr.c
@@ -5,7 +5,6 @@
 #include <scheduler.h>
 #include <chipselect.h>
 
-
 static bool __idle(void* user_data)
 {
         return true;
@@ -17,6 +16,10 @@ int main()
         cs2_sys_init();
         spiioexp_sys_init();
         extsram_sys_init();
+
+        // spiioexp_test_blink_led();
+        // extsram_test_read_write();
+        // hidusb_print_test();
 
         schd_run(__idle, nullptr);
         return 0;
