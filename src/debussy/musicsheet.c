93269815023bc112fa4fa0e95dfd32a4d929f23d
Chifeng Wen <daviesx66@gmail.com>
2016-06-24 18:44:33 -0700

    rename the music sheet class.

--- src/debussy/musicsheet.c
+++ src/debussy/musicsheet.c
@@ -1,60 +1,88 @@
 #include <filesystem.h>
 #include <memory.h>
+#include <string.h>
 #include <musicsheet.h>
 
 
 /*
- * <musicsheet> public
+ * <music_sheet> public
  */
-void musicsheet_init(struct musicsheet* self)
+void music_sheet_init(struct music_sheet* self)
+{
+        memset(self, 0, sizeof(*self));
+}
+
+void music_sheet_free(struct music_sheet* self)
+{
+        memset(self, 0, sizeof(*self));
+}
+
+void music_sheet_load_from_midi_file(struct music_sheet* self, struct file* file)
 {
 }
 
-void musicsheet_free(struct musicsheet* self)
-{
-}
-
-void musicsheet_load_from_midi_file(struct musicsheet* self, struct file* file)
-{
-}
-
-struct note* musicsheet_get_notes(struct musicsheet* self, uint32_t* num_notes)
+struct note* music_sheet_get_notes(struct music_sheet* self, uint32_t* num_notes)
 {
         return nullptr;
 }
 
-float musicsheet_get_time_length(struct musicsheet* self)
+float music_sheet_get_time_length(struct music_sheet* self)
 {
         return 0.0f;
 }
 
-void musicsheet_get_iter_by_progress(struct musicsheet* self, float progress, struct musicsheet_iter* iter)
+void music_sheet_get_iter_by_progress(struct music_sheet* self, float progress, struct music_sheet_iter* iter)
 {
 }
 
-void musicsheet_get_iter_by_note_id(struct musicsheet* self, uint32_t note_id, struct musicsheet_iter* iter)
+void music_sheet_get_iter_by_note_id(struct music_sheet* self, uint32_t note_id, struct music_sheet_iter* iter)
 {
 }
 
-void musicsheet_insert_note_at(struct musicsheet* self, struct musicsheet_iter* iter, struct note* note)
+void music_sheet_insert_note_at(struct music_sheet* self, struct music_sheet_iter* iter, struct note* note)
 {
 }
 
-void musicsheet_remove_note_at(struct musicsheet* self, struct musicsheet_iter* iter)
+void music_sheet_remove_note_at(struct music_sheet* self, struct music_sheet_iter* iter)
 {
 }
 
-void musicsheet_play_next(struct musicsheet* self, struct musicsheet_iter* iter, struct synth* synth)
+bool music_sheet_play_next(struct music_sheet* self, struct music_sheet_iter* iter, struct synth* synth)
 {
+        return false;
 }
 
-void musicsheet_set_tempo(struct musicsheet* self, uint8_t bpm)
+void music_sheet_set_tempo(struct music_sheet* self, uint8_t bpm)
 {
 }
 
 /*
- * <musicsheet> test cases
+ * <music_sheet> test cases
  */
-void musicsheet_test_play_midi_file(struct file* file)
+#include <filesystem.h>
+#include <synthesizer.h>
+
+void music_sheet_test_play_midi_file()
 {
+        struct fs_posix fs;
+        fs_posix_init(&fs, "data");
+        struct file* file = filesys_open_file(&fs.__parent, "./piano_sonata_13_1_(c)oguri.mid", false);
+
+        struct music_sheet sheet;
+        music_sheet_init(&sheet);
+        music_sheet_load_from_midi_file(&sheet, file);
+
+        filesys_free(&fs.__parent);
+
+        struct music_sheet_iter iter;
+        music_sheet_get_iter_by_note_id(&sheet, 0, &iter);
+
+        struct syn_audioif syn;
+        syn_aif_init(&syn);
+
+        while (music_sheet_play_next(&sheet, &iter, &syn.__parent))
+                continue;
+
+        music_sheet_free(&sheet);
+        syn_aif_free(&syn);
 }
--- src/debussy/musicsheet.h
+++ src/debussy/musicsheet.h
@@ -2,42 +2,54 @@
 #define MUSICSHEET_H_INCLUDED
 
 #include <types.h>
+#include <memory.h>
 #include <synthesizer.h>
 
-
-/*
- * <musicsheet_iter>
- */
-struct musicsheet_iter {
+struct note {
+        uint8_t         i_semi;
+        uint8_t         length;
+        uint8_t         force;
 };
 
 /*
- * <musicsheet> decl
+ * <music_sheet_iter>
+ */
+struct music_sheet_iter {
+        int             n_ticks;
+};
+
+/*
+ * <music_sheet> decl
  */
 struct file;
-struct note;
-struct musicsheet {
+struct music_sheet {
+        struct note*    note_q;
+        uint16_t        front;
+        uint16_t        rear;
+        mem_addr_t      all_notes;
+        uint16_t        n_notes;
+        uint16_t        tempo;
 };
 
 /*
- * <musicsheet> public
+ * <music_sheet> public
  */
-void            musicsheet_init(struct musicsheet* self);
-void            musicsheet_free(struct musicsheet* self);
-void            musicsheet_load_from_midi_file(struct musicsheet* self, struct file* file);
-struct note*    musicsheet_get_notes(struct musicsheet* self, uint32_t* num_notes);
-float           musicsheet_get_time_length(struct musicsheet* self);
-void            musicsheet_get_iter_by_progress(struct musicsheet* self, float progress, struct musicsheet_iter* iter);
-void            musicsheet_get_iter_by_note_id(struct musicsheet* self, uint32_t note_id, struct musicsheet_iter* iter);
-void            musicsheet_insert_note_at(struct musicsheet* self, struct musicsheet_iter* iter, struct note* note);
-void            musicsheet_remove_note_at(struct musicsheet* self, struct musicsheet_iter* iter);
-void            musicsheet_play_next(struct musicsheet* self, struct musicsheet_iter* iter, struct synth* synth);
-void            musicsheet_set_tempo(struct musicsheet* self, uint8_t bpm);
+void            music_sheet_init(struct music_sheet* self);
+void            music_sheet_free(struct music_sheet* self);
+void            music_sheet_load_from_midi_file(struct music_sheet* self, struct file* file);
+struct note*    music_sheet_get_notes(struct music_sheet* self, uint32_t* num_notes);
+float           music_sheet_get_time_length(struct music_sheet* self);
+void            music_sheet_get_iter_by_progress(struct music_sheet* self, float progress, struct music_sheet_iter* iter);
+void            music_sheet_get_iter_by_note_id(struct music_sheet* self, uint32_t note_id, struct music_sheet_iter* iter);
+void            music_sheet_insert_note_at(struct music_sheet* self, struct music_sheet_iter* iter, struct note* note);
+void            music_sheet_remove_note_at(struct music_sheet* self, struct music_sheet_iter* iter);
+bool            music_sheet_play_next(struct music_sheet* self, struct music_sheet_iter* iter, struct synth* synth);
+void            music_sheet_set_tempo(struct music_sheet* self, uint8_t bpm);
 
 /*
- * <musicsheet> test cases
+ * <music_sheet> test cases
  */
-void            musicsheet_test_play_midi_file(struct file* file);
+void            music_sheet_test_play_midi_file();
 
 
 #endif // MUSICSHEET_H_INCLUDED
