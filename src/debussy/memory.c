bd2f45a2f6bbe8bffa95ef86ebac3e3a65deb254
Chifeng Wen <daviesx66@gmail.com>
2016-06-24 17:10:24 -0700

    implemented memory under x86_64.

--- src/debussy/memory.c
+++ src/debussy/memory.c
@@ -2,5 +2,34 @@
 #include <extsram.h>
 #include <memory.h>
 
-#define EXTSRAM0        CS_LATCH2
-#define EXTSRAM1        CS_LATCH3
+
+#ifndef ARCH_X86_64
+#else
+#  include <stdlib.h>
+#  include <string.h>
+
+/*
+ * <memory> public
+ */
+mem_addr_t mem_alloc(size_t n_bytes)
+{
+        return malloc(n_bytes);
+}
+
+void mem_free(mem_addr_t addr)
+{
+        free(addr);
+}
+
+void mem_read(mem_addr_t addr, size_t n_bytes, void* buf)
+{
+        memcpy(buf, addr, n_bytes);
+}
+
+void mem_write(mem_addr_t addr, size_t n_bytes, const void* buf)
+{
+        memcpy(addr, buf, n_bytes);
+}
+
+
+#endif // ARCH_X86_64
--- src/debussy/memory.h
+++ src/debussy/memory.h
@@ -2,5 +2,33 @@
 #define MEMORY_H_INCLUDED
 
 
+#include <types.h>
+
+#ifndef ARCH_X86_64
+typedef struct {
+        union {
+                struct {
+                        uint8_t addr0_7;
+                        uint8_t addr8_15;
+                        uint8_t addr16_18;
+                };
+                uint32_t        addr;
+                uint8_t         addr_parts[3];
+        };
+} mem_addr_t;
+#else
+typedef void*   mem_addr_t;
+
+/*
+ * <memory> public
+ */
+mem_addr_t      mem_alloc(size_t n_bytes);
+void            mem_free(mem_addr_t addr);
+void            mem_read(mem_addr_t addr, size_t n_bytes, void* buf);
+void            mem_write(mem_addr_t addr, size_t n_bytes, const void* buf);
+
+#endif // ARCH_X86_64
+
+
 
 #endif // MEMORY_H_INCLUDED
